599e06d36b1721635f90ff30702d053792e12b40
Ulisses <ualbardeiro@gmail.com>
2026-01-29 18:36:55 -0300

    feat: install bootstrap via assetmapper

--- assets/app.js
+++ assets/app.js
@@ -6,5 +6,5 @@ import './stimulus_bootstrap.js';
  * which should already be in your base.html.twig.
  */
 import './styles/app.css';
-
-console.log('This log comes from assets/app.js - welcome to AssetMapper! 🎉');
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap';
